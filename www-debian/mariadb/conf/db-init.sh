81aefcbede0182db293bc13ca081664dafc38b68
itd3vbox <it.d3vbox@gmail.com>
2024-09-17 23:09:28 +0200

    some update - better version

--- www-debian/mariadb/conf/db-init.sh
+++ www-debian/mariadb/conf/db-init.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-/etc/init.d/mysql start
+# Display the root password (remove this after debugging)
+echo "MariaDB root password: ${DB_ROOT_PASSWORD}"
 
-while ! mysqladmin ping -hlocalhost -uroot -proot --silent; do
-    echo "Attente du démarrage du service MariaDB..."
+# Start the MariaDB service
+echo "Starting the MariaDB service..."
+mysqld_safe &
+
+# Wait for MariaDB to be ready
+echo "MariaDB service started, waiting for availability..."
+while ! mysqladmin ping -hlocalhost -uroot -p${DB_ROOT_PASSWORD} --silent; do
+    echo "Waiting for MariaDB service to start..."
     sleep 1
 done
 
-mysql -u root -proot < /db.sql
+# Execute the SQL script to initialize the database
+echo "MariaDB is ready, initializing the database..."
+mysql -u root -p${DB_ROOT_PASSWORD} < /db.sql
+if [ $? -ne 0 ]; then
+    echo "Error executing SQL script."
+    exit 1
+fi
 
-tail -f /dev/null
+echo "SQL script executed successfully."
+
+# Stop the MariaDB service after initialization
+echo "Stopping the MariaDB service after initialization..."
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+if [ $? -ne 0 ]; then
+    echo "Error stopping the MariaDB service."
+    exit 1
+fi
+
+# Ensure MariaDB is fully stopped
+if mysqladmin ping -hlocalhost --silent; then
+    echo "MariaDB did not stop properly."
+    exit 1
+fi
+
+echo "MariaDB stopped successfully."
--- www-debian/mariadb/conf/db.sql
+++ www-debian/mariadb/conf/db.sql
@@ -1,12 +1,28 @@
+-- USE mysql;
+
+-- UPDATE user SET authentication_string=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${DB_ROOT_PASSWORD}');
+
+FLUSH PRIVILEGES;
+
+-- Create the databases if they do not exist
 CREATE DATABASE IF NOT EXISTS `${DB_NAME}`;
 CREATE DATABASE IF NOT EXISTS `${DB_TEST_NAME}`;
 
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-CREATE USER '${DB_USER_ADMIN}'@'%' IDENTIFIED BY '${DB_PASSWORD_ADMIN}';
+-- Create the users
+CREATE user '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+CREATE user '${DB_USER_ADMIN}'@'%' IDENTIFIED BY '${DB_PASSWORD_ADMIN}';
+
+-- Grant all privileges on the databases to the users
 GRANT ALL PRIVILEGES ON `${DB_NAME}`.* TO '${DB_USER_ADMIN}'@'%';
 GRANT ALL PRIVILEGES ON `${DB_NAME}`.* TO '${DB_USER}'@'%';
 GRANT ALL PRIVILEGES ON `${DB_TEST_NAME}`.* TO '${DB_USER_ADMIN}'@'%';
 GRANT ALL PRIVILEGES ON `${DB_TEST_NAME}`.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+-- Update the password for the root user
+-- SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${DB_ROOT_PASSWORD}');
+-- ALTER user 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+-- UPDATE user SET authentication_string=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';
+
+-- Refresh privileges
+FLUSH PRIVILEGES;
